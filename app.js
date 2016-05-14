8722263c20d71b246b7c469d0c50055e233d6a2f
José Rodolfo Freitas <joserodolfo.freitas@gmail.com>
2016-05-14 08:37:21 +0200

    VoteBox as a service

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    voteBox = require('./lib/vote-box');
+
+app.use(bodyParser.json());
+
+app.post('/vote', function(req, res) {
+    var vote = req.body;
+    console.log(vote);
+    var top5Candidates = vote.top5;
+    var tituloDeEleitor = vote.titulo;
+
+    voteBox.receiveVote(top5Candidates, tituloDeEleitor, function(err, result) {
+        if(err) {
+            res.status(500).json({err: err});
+        }
+        res.status(200).json({success: true});
+    });
+});
+
+app.listen(3000, function() {
+    console.log('listening on port 3000')
+});
